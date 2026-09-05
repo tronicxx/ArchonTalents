c07aa7ebec12391fb9a7aaeca9c0d2682598a55a
tronicxx <117591732+tronicxx@users.noreply.github.com>
2026-09-05 08:09:59 -0500

    Update talent data for version 2.0.1788613799

--- ArchonData.lua
+++ ArchonData.lua
@@ -1,13 +1,13 @@
 -- Archon Talents Data File
--- Generated: 2026-09-04 08:03:04 UTC
--- Version: 1788526984
+-- Generated: 2026-09-05 08:03:01 UTC
+-- Version: 1788613381
 -- Source: Archon.gg Meta Builds
 -- This is a plain global table, NOT a SavedVariable.
 -- It gets freshly loaded from this file every time the addon loads.
 
 ArchonTalentsData = {
-    version = 1788526984,
-    lastUpdated = "2026-09-04 08:03:04 UTC",
+    version = 1788613381,
+    lastUpdated = "2026-09-05 08:03:01 UTC",
     specData = {
         ["Imperator"] = {
         },
